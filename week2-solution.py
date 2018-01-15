0db159082a192f36fb374766fe2198432a8f39a7
Sean Stoneburner <litholight@gmail.com>
2018-01-15 13:22:19 -0500

    Redefined function arguments to account for unknown argument number

--- week2-solution.py
+++ week2-solution.py
@@ -1,7 +1,13 @@
 
 def myrange2(first, second=None, step=1):
-    print("first = {}".format(first))
-    print("second = {}".format(second))
+    if second is None:
+        current = 0
+        end = first
+    else:
+        current = first
+        end = second
+    print("first = {}".format(current))
+    print("second = {}".format(end))
     print("step = {}".format(step))
 
 myrange2(10)
