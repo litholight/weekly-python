998a92e476735d46f33287f6c6e3afdd03ff4e79
Sean Stoneburner <litholight@gmail.com>
2018-01-15 13:10:50 -0500

    Identify and test the three parameters.

---
+++ week2-solution.py
@@ -0,0 +1,12 @@
+
+def myrange2(first, second=None, step=1):
+    print("first = {}".format(first))
+    print("second = {}".format(second))
+    print("step = {}".format(step))
+
+myrange2(10)
+
+myrange2(10,20)
+
+myrange2(10,20,2)
+
