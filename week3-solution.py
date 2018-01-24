bbd27cf35869ddc984647c577ba51cb7b48958eb
Sean Stoneburner <litholight@gmail.com>
2018-01-23 22:39:31 -0500

    Used str.split and str.index methods to find indexes for log lines.

--- week3-solution.py
+++ week3-solution.py
@@ -1,6 +1,17 @@
+def line_to_dict(line):
+    
+    ip_address = line.split()[0]
 
+    timestamp_start = line.index('[') + 1
+    timestamp_end = line.index(']')
+    timestamp = line[timestamp_start:timestamp_end]
+
+    request_start = line.index('"') + 1
+    request_end = line[request_start:].index('"')
+    request = line[request_start:request_start+request_end]
+
+    return {'ip address':ip_address, 'timestamp':timestamp, 'request':request} 
 def logtodict(filename):
-    return[{'':''}
-            for line in open(filename)]
+    return[line_to_dict(line) for line in open(filename)]
 
 print(logtodict('mini-access-log.txt'))
