bcd82b790ae981ff9e868d56ff6f19dd73edad92
Sean Stoneburner <litholight@gmail.com>
2018-02-22 20:25:42 -0500

    created outer function to take in dir and any file function.

--- week8.py
+++ week8.py
@@ -1,14 +1,29 @@
 import os
 from collections import defaultdict
 
-def file_length(directory):
-    filelength = defaultdict()
+def file_function(directory, any_file_func):
+    success_dict = {}
+    failure_dict = {}
     for filename in os.listdir(directory):
-        size = os.stat(filename).st_size
-        filelength[filename] = size
-    print(filelength)
+        if os.path.isdir(filename):
+            failure_dict[filename] = "Exception 1"
+            break
+        success_dict[filename] = any_file_func(filename)
+    return success_dict, failure_dict
 
-file_length("./")
+def file_length(filename):
+    size = os.stat(filename).st_size
+    return size
 
-#filename = "week7printout.txt"
-#print(os.stat(filename).st_size)
+directory = "./"
+
+success_dict, failure_dict = file_function(directory, file_length)
+
+print("Filenames and the function result")
+for k,v in success_dict.items():
+    print(k, ":", v)
+
+print("Filenames and exception number:")
+for k,v in failure_dict.items():
+    print(k, ":", v)
+
