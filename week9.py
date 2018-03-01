e11605bf9a0271c038d1f98a68c1db76f7aee709
Sean Stoneburner <litholight@gmail.com>
2018-02-28 22:12:38 -0500

    fixed format and output to txt file.

--- week9.py
+++ week9.py
@@ -3,14 +3,13 @@ import random
 
 def rand_forty():
     for x in range(20):
-        return "( " + str(random.randint(-40,40)) + " )"
-
+        return "( " + str(random.randint(-40,40)) + ")"
     
 randop = [" - ", " + "]
-   
 
-
-
+file = open('mathProblems.txt','w')
 
 for x in range(100):
-    print(rand_forty() + random.choice(randop) +  rand_forty() + random.choice(randop) + rand_forty() + random.choice(randop) + rand_forty())
+    file.write("[ " + str(x+1) + "] " + rand_forty() + random.choice(randop) +  rand_forty() + 
+            random.choice(randop) + rand_forty() + random.choice(randop) + rand_forty() + " = ________\n")
+file.close()
