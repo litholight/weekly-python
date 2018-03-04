0ce5572e4c50369f1c32845a6d1dd538463d6c3a
Sean Stoneburner <litholight@gmail.com>
2018-03-04 15:24:47 -0500

    added formatting to problems.

--- week9.py
+++ week9.py
@@ -3,13 +3,13 @@ import random
 
 def rand_forty():
     for x in range(20):
-        return "( " + str(random.randint(-40,40)) + ")"
+        return "(" + '{:3}'.format(random.randint(-40,40)) + ")"
     
 randop = [" - ", " + "]
 
 file = open('mathProblems.txt','w')
 
 for x in range(100):
-    file.write("[ " + str(x+1) + "] " + rand_forty() + random.choice(randop) +  rand_forty() + 
+    file.write("[" + '{:3d}'.format(x+1) + "] " + rand_forty() + random.choice(randop) +  rand_forty() + 
             random.choice(randop) + rand_forty() + random.choice(randop) + rand_forty() + " = ________\n")
 file.close()
