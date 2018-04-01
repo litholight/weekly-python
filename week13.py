def6a9b4797a816f20482849522bda3d0aeb5a95
Sean Stoneburner <litholight@gmail.com>
2018-04-01 11:08:41 -0400

    added the history method to RandMemory class

--- week13.py
+++ week13.py
@@ -2,16 +2,24 @@ from random import randint
 
 class RandMemory(object):
 
+    
     def __init__(self,minimum,maximum):
-
+        self.listri = []
         self.minimum = minimum
         self.maximum = maximum
 
     @property
     def get(self):
-        return randint(self.minimum, self.maximum)        
+        self.ri = randint(self.minimum, self.maximum)        
+        self.listri.append(self.ri) 
+        return self.ri
+
+    def history(self):
+        print(self.listri)
 
 r = RandMemory(1, 100)
 print(r.get)
 print(r.get)
 print(r.get)
+r.history()
+
